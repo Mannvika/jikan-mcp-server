b1d0b62201162afb5c028aa5193d00d2f1217afb
Mannvika <45762765+Mannvika@users.noreply.github.com>
2026-03-23 13:57:05 -0400

    Configured typescript and added initial types

---
+++ src/types/jikan.ts
@@ -0,0 +1,52 @@
+// Anime and Manga nodes from Jikan API
+export interface AnimeNode {
+    mal_id: number;
+    name: string;
+    type: string; // Anime or Manga
+    url: string;
+}
+
+export interface Relation{
+    relation: string; // e.g., "Adaptation", "Prequel", "Sequel"
+    entry: AnimeNode[];
+}
+
+export interface AnimeDetails {
+    mal_id: number;
+    title: string;
+    title_english: string | null;
+    type: string; // e.g., "TV", "Movie", "OVA"
+    synopsis: string | null;
+    aired: {
+        from: string | null; // ISO 8601 date
+        to: string | null; // ISO 8601 date
+    };
+    relations?: Relation[];
+}
+
+// API Wrappers (Jikan wraps respones in data objects)
+export interface JikanSearchResponse{
+    data: AnimeDetails[];
+}
+
+export interface JikanAnimeResponse{
+    data: AnimeDetails;
+}
+
+export interface JikanRelationResponse{
+    data: Relation[];
+}
+
+// Merge Franchise Tool
+export interface MergedFranchise{
+    franchies_name: string;
+    root_mal_id: number;
+    total_entries: number;
+    entries: {
+        mal_id: number;
+        title: string;
+        type: string;
+        release_data: string | null;
+        relation_to_root: string;
+    }[]
+}
